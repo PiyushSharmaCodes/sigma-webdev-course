38fa464a1e29eccb533fdf8bb3963904686a1507
Piyush Sharma <piyushcodes123@gmail.com>
2024-05-22 18:41:12 +0530

    all SVG files seperated in a folder

---
+++ Javascript/video55/script.js
@@ -0,0 +1,31 @@
+console.log("hey this is tutorial 55 of sigma web dev course")
+var a = 5;
+var b = 8;
+var c = "Sharma ji"
+console.log(typeof(a))
+console.log(typeof(c))
+console.log(a+b)
+
+let v1=null
+let v2=20
+let v3 = 3.56
+let v4="hello world"
+let v5=true
+let v6=undefined
+let v7=BigInt(1234567890123456789012345)
+const v8=true
+
+let person={
+    name:"Piyush Sharma",
+    age:20
+}
+console.log(v1,v2,v3,v4,v5,v6,v7,v8)
+console.log(typeof v1,typeof v2,typeof v3,typeof v4,typeof v5,typeof v6,typeof v7,typeof v8)
+
+console.log(person)
+
+person.salary="500crore"
+console.log(person)
+
+person.hobby="music"
+console.log(person.name + "'s hobby is " + person.hobby)
