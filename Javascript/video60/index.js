f99239564e17ed261bd271bffd98be0d9c4006a7
Piyush Sharma <piyushcodes123@gmail.com>
2024-06-17 23:55:33 +0530

    Some usefull functions of string added

---
+++ Javascript/video60/index.js
@@ -0,0 +1,9 @@
+let myname = "Piyush";
+let friend = "Nakshatra Raghav";
+
+console.log(`I am ${myname} and my friend's name is ${friend.toUpperCase()}`);
+
+console.log(`My full name is ${myname.concat(" Sharma")}`);
+console.log(`My friend's nickname is ${friend.slice(10)}`)
+console.log(`His another name is ${friend.replace("Raghav","Rastogi")}`)
+console.log(`Length of my name is ${myname.length}`)
