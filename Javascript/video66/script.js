4dfe4458b59269e1ca32dfd0d0f1e7f769f123ee
Piyush Sharma <piyushcodes123@gmail.com>
2024-06-21 20:27:25 +0530

    DOM added

---
+++ Javascript/video66/script.js
@@ -0,0 +1,7 @@
+document.title="Piyush Bhaii DOM sikh rhe hain";
+    // Select the element with the class 'box'
+    let box = document.getElementsByClassName('box');
+
+    // Change its color to blue
+    box[0].style.color = 'red';
+    box[0].style.backgroundColor = 'yellow';
