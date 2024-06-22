491cc67f0f776278f403e202f9ff18c1e9313abc
Piyush Sharma <piyushcodes123@gmail.com>
2024-06-23 00:11:10 +0530

    Code added for selecting elements by id,class and querySelector method

---
+++ Javascript/video68/script.js
@@ -0,0 +1,9 @@
+// let boxes = document.getElementsByClassName("box");
+// boxes[2].style.backgroundColor="blue";
+
+// let redbox = document.getElementById("redBox");
+// redbox.style.backgroundColor ="red";
+
+// document.querySelectorAll(".box").forEach(e => {
+//     e.style.backgroundColor="yellow";
+// })
