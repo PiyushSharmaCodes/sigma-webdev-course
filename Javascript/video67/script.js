de10970a91d734ed248a1cf88771c0d783898f17
Piyush Sharma <piyushcodes123@gmail.com>
2024-06-22 23:32:05 +0530

    children parent and sibling nodes in javascipt DOM

---
+++ Javascript/video67/script.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+document.body.firstElementChild;
+document.body.firstElementChild.childNodes;
+document.body.childNodes;
+document.body.childNodes[1].firstElementChild.style.backgroundColor='green';
+document.body.firstElementChild.children[3].style.backgroundColor="violet";
