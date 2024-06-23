efc01cfa03001926cb7bbb450e6b568136aed0a6
Piyush Sharma <piyushcodes123@gmail.com>
2024-06-24 00:32:08 +0530

    code for removal and insertion of element using javascript

---
+++ Javascript/video71/script.js
@@ -0,0 +1,22 @@
+// document.querySelector(".box")
+// <div class=​"box">​Hey I am a box​</div>​
+// document.querySelector(".box").inn
+// undefined
+// document.querySelector(".box").innerHTML
+// 'Hey I am a box'
+// document.querySelector(".box").innerText
+// 'Hey I am a box'
+// document.querySelector(".container").innerText
+// 'Hey I am a box'
+// document.querySelector(".container").outerHTML
+// '<div class="container">\n        <div class="box">Hey I am a box</div>\n    </div>'
+// document.querySelector(".container").tag
+// undefined
+// document.querySelector(".container").tagName
+// 'DIV'
+// document.querySelector(".container").hidden=true
+// true
+// document.querySelector(".container").hidden=false
+// false
+// document.querySelector(".box").innerHTML="Hey I am Piyush Sharma"
+// 'Hey I am Piyush Sharma'
